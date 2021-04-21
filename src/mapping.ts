36dcca31d397043a25a579259edf317386d41307
Cui Qin <qincuidhu@gmail.com>
2021-04-21 20:39:19 +0200

    change to the load method

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,7 +3,7 @@ import { PolkamonOwner } from '../generated/schema'
 
 export function handleTransfer(event: Transfer): void {
     let id = event.params.tokenId.toHex()
-    let polkamon = new PolkamonOwner(id)
+    let polkamon = PolkamonOwner.load(id)
     if (polkamon == null) {
         polkamon = new PolkamonOwner(id)
     }
