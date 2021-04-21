0061b1f2cfafa7198a10fa0d5b8371d02f35cc14
Cui Qin <qincuidhu@gmail.com>
2021-04-21 19:44:08 +0200

    changed the variable name

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,10 +3,10 @@ import { PolkamonOwner } from '../generated/schema'
 
 export function handleTransfer(event: Transfer): void {
     let id = event.params.tokenId.toHex()
-    let kitty = new PolkamonOwner(id)
-    if (kitty == null) {
-        kitty = new PolkamonOwner(id)
+    let polkamon = new PolkamonOwner(id)
+    if (polkamon == null) {
+        polkamon = new PolkamonOwner(id)
     }
-    kitty.owner = event.params.to
-    kitty.save()
+    polkamon.owner = event.params.to
+    polkamon.save()
 }
