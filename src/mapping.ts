732b86113b934cba65446ff28653d52d76838681
Cui Qin <qincuidhu@gmail.com>
2021-04-25 11:21:25 +0200

    index the amount of the token owned by the user

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,6 @@
 import { Transfer } from '../generated/Polkamon/Polkamon'
-import { PolkamonOwner } from '../generated/schema'
+import { PolkamonOwner, PolkamonBalance } from '../generated/schema'
+import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleTransfer(event: Transfer): void {
     let id = event.params.tokenId.toHex()
@@ -9,4 +10,25 @@ export function handleTransfer(event: Transfer): void {
     }
     polkamon.owner = event.params.to
     polkamon.save()
+
+    let previousOwner = event.params.from.toHex()
+    let polkamonBalance = PolkamonBalance.load(previousOwner)
+    if (polkamonBalance != null) {
+        if (polkamonBalance.amount > BigInt.fromI32(0)) {
+            polkamonBalance.amount = polkamonBalance.amount - BigInt.fromI32(1)
+        }
+    } else {
+        polkamonBalance = new PolkamonBalance(previousOwner)
+        polkamonBalance.amount = BigInt.fromI32(0)
+    }
+    polkamonBalance.save()
+
+    let newOwner = event.params.to.toHex()
+    let newPolkamonBalance = PolkamonBalance.load(newOwner)
+    if (newPolkamonBalance == null) {
+        newPolkamonBalance = new PolkamonBalance(newOwner)
+        newPolkamonBalance.amount = BigInt.fromI32(0)
+    }
+    newPolkamonBalance.amount = newPolkamonBalance.amount + BigInt.fromI32(1)
+    newPolkamonBalance.save()
 }
