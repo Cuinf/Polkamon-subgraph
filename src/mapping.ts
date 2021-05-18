d32ce57c458eaa95989dc289f0111c824992cad7
Cui Qin <qincuidhu@gmail.com>
2021-05-18 18:48:23 +0200

    changed to use the contract binding

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,13 +1,11 @@
-import { Transfer, MintCall } from '../generated/Polkamon/Polkamon'
+import { Transfer, MintCall, Polkamon } from '../generated/Polkamon/Polkamon'
 import { PolkamonOwner, PolkamonBalance, TransferTrace } from '../generated/schema'
 import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleTransfer(event: Transfer): void {
     let id = event.params.tokenId.toHex()
-    let polkamon = PolkamonOwner.load(id)
-    if (polkamon == null) {
-        polkamon = new PolkamonOwner(id)
-    }
+    let polkamon = new PolkamonOwner(id)
+    
     polkamon.tokenId = event.params.tokenId
     polkamon.owner = event.params.to
     polkamon.contract = event.address
@@ -20,24 +18,11 @@ export function handleTransfer(event: Transfer): void {
     transferEntity.timestamp = event.block.timestamp
     transferEntity.save()
 
-    //count the amount of tokens hold by an owner
-    let previousOwner = event.params.from.toHex()
-    let polkamonBalance = PolkamonBalance.load(previousOwner)
-    if (polkamonBalance != null) {
-        if (polkamonBalance.amount > BigInt.fromI32(0)) {
-            polkamonBalance.amount = polkamonBalance.amount - BigInt.fromI32(1)
-        }
-        polkamonBalance.save()
-    }    
-
-    let newOwner = event.params.to.toHex()
-    let newPolkamonBalance = PolkamonBalance.load(newOwner)
-    if (newPolkamonBalance == null) {
-        newPolkamonBalance = new PolkamonBalance(newOwner)
-        newPolkamonBalance.amount = BigInt.fromI32(0)
-    }
-    newPolkamonBalance.amount = newPolkamonBalance.amount + BigInt.fromI32(1)
-    newPolkamonBalance.save()
+    //update the amount of tokens hold by an owner
+    let contract = Polkamon.bind(event.address)
+    let polkamonBalance = new PolkamonBalance(event.params.to.toHex())
+    polkamonBalance.amount = contract.balanceOf(event.params.to)
+    polkamonBalance.save()
 }
 
 export function handleMint(call: MintCall): void {
